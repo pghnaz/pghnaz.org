4e631090e718b9c0aac76e720d79da8d54a09d0c
Reuben L. Lillie <reubenlillie@gmail.com>
2019-07-16 13:58:34 -0500

    Add fileToString and minifyCSS to 11ty config and update theme

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,30 @@
+/**
+ * @file Eleventy’s configuration file
+ * @author Reuben L. Lillie <rlilie@pghnaz.org>
+ */
+
+// Require local modules
+var fileToString = require('./src/includes/filters/file-to-string')
+var minifyCSS = require('./src/includes/filters/minify-css')
+
+/**
+ * Expose the configuration to 11ty as a function
+ * @module .eleventy
+ * @param 11ty’s config argument
+ * @see {@link https://www.11ty.io/docs/config/ 11ty docs}
+ */
 module.exports = function (eleventyConfig) {
 
+
+	// Pass 11ty config argument to local modules
+	fileToString(eleventyConfig)
+	minifyCSS(eleventyConfig)
+
 	/**
-	 * Copy static assets directly from input to output
+	 * Copy static assets directly from includes to output
 	 * @see {@link https://www.11ty.io/docs/copy/ 11ty docs}
 	 */
-	eleventyConfig.addPassthroughCopy('assets')
+	eleventyConfig.addPassthroughCopy('src/includes/assets')
 
 	/**
 	 * Overwrite 11ty’s default configuration options
