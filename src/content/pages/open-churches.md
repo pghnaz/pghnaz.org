6a1c812d914e2e65a6f396dec2f46200d6866060
Reuben L. Lillie <reubenlillie@gmail.com>
2019-08-01 22:55:00 -0500

    Add information for open churches

---
+++ src/content/pages/open-churches.md
@@ -0,0 +1,12 @@
+---
+title: Open Churches
+layout: open-churches
+---
+
+Please pray with us for these local churches as they seek their next pastor.
+
+If you or someone you know are interested in becoming a pastoral candidate for one of these local churches, then please [contact us](/contact/).
+
+Check out our [Church Directory](/directory/) or our interactive [District Map](/map/) for a full list of local churches.
+
+_listed alphabetically by local church name_
--- src/layouts/church-directory.11ty.js
+++ src/layouts/church-directory.11ty.js
@@ -16,53 +16,13 @@ class ChurchDirectory {
 	}
 
 	render(data) {
-		var arr = Object.keys(data.churches)
-						.map(item => data.churches[item])
+		// Create an array from a JavaScript object containing keyed objects
+		var churches =  this.arrayFromObject(data.churches)
+
 		return `
 			${data.content}
 			<section class="grid">
-				${arr.map(item => {
-					var id = item.properties.gmcID.substr(4)
-					var href = `/${Object.keys(data.churches).find(key => data.churches[key] === item)}`
-					return `
-					<article class="border border-radius more-padding margin shadow" id="church_${id}">
-						<header>
-							<h1>
-								<a href="${href}">
-									<span id="churchName_${id}">
-										${item.properties.name}
-									</span>
-								</a>
-							</h1>
-						</header>
-						<section>
-							<address>
-								${item.properties.address
-									? `
-										<p>
-											<span>${item.properties.address.street}</span></br>
-											<span id="churchCity_${id}">${item.properties.address.city}</span>,
-											<span>${item.properties.address.state}</span>
-											<span>${item.properties.address.zip}</span>
-
-										</p>
-									`
-									: ''
-								}
-							</address>
-							<p>
-								${item.properties.pastor
-									? `Pastor ${item.properties.pastor}`
-									: 'Praying for our next pastor'
-								}
-							</p>
-						</section>
-						<footer class="small">
-							<p><a href="${href}">${data.site.baseURL}${href.substr(1)}</a></p>
-						</footer>
-					</article>
-				`
-			}).join('')}
+				${churches.map(church => this.localChurchCard(data, church)).join('')}
 			</section>
 		`
 	}
